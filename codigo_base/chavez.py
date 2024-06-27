fbdef2588bd68f918bde04ed2e384b17aca3d120
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-06-27 09:02:39 -0600

    Ejercicio 12, seccion a: Se agrego el histograma usando seis clases

--- codigo_base/chavez.py
+++ codigo_base/chavez.py
@@ -2,7 +2,7 @@ import math
 
 def clases_grouped(datos):
     amplitud = round(max(datos) - min(datos), 2)
-    nclases = 1 + (3.3*math.log10(len(datos)))
+    nclases = round(1 + (3.3*math.log10(len(datos))))
     anc_clas = round(amplitud / math.floor(nclases), 2)
 
     marc_clase = []
@@ -14,4 +14,4 @@ def clases_grouped(datos):
         lim_inf.append(round(min(datos) + i*anc_clas, 2))
         lim_sup.append(round(min(datos) + (i+1)*anc_clas, 2))
 
-    return lim_inf, lim_sup, marc_clase
+    return lim_inf, lim_sup, marc_clase, nclases
